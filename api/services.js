b6a942ad1e3d2fdb45ede51644d4548e959112ca
alpianoor352-ux <alpianoor352@gmail.com>
2026-07-16 12:45:55 +0800

    Update services.js

--- api/services.js
+++ api/services.js
@@ -1,25 +1,4 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("https://ordersosmed.id/api-1/service", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        api_id: process.env.API_ID,
-        api_key: process.env.API_KEY,
-        secret_key: process.env.SECRET_KEY
-      })
-    });
+const text = await response.text();
+console.log(text);
 
-    const data = await response.json();
-
-    res.status(200).json(data);
-
-  } catch (err) {
-    res.status(500).json({
-      response: false,
-      msg: err.message
-    });
-  }
-}
+res.status(200).send(text);
