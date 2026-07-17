158246667bdbf7dbc91a1b25cc2ea1467854c72d
alpianoor352-ux <alpianoor352@gmail.com>
2026-07-17 10:57:05 +0800

    Update services.js

--- api/services.js
+++ api/services.js
@@ -15,8 +15,12 @@ export default async function handler(req, res) {
     });
 
     const text = await response.text();
-    res.status(200).send(text);
 
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+res.status(200).send(text);
   } catch (err) {
     res.status(500).json({
       response: false,
