e586faf1a1df6cd61d055b095521d0538ae9ea0d
alpianoor352-ux <alpianoor352@gmail.com>
2026-07-24 12:32:11 +0800

    Create nokos-country.js

---
+++ api/nokos-country.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+
+  try {
+
+    const response = await fetch(
+      "https://nokos.co.id/api/?action=getCountries",
+      {
+        headers: {
+          "X-API-Key": process.env.NOKOS_API_KEY
+        }
+      }
+    );
+
+    const result = await response.json();
+
+    return res.status(200).json(result);
+
+  } catch (err) {
+
+    return res.status(500).json({
+      success: false,
+      message: err.message
+    });
+
+  }
+
+      }
