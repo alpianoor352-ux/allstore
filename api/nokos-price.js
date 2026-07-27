da1c23834d41d24c21c9359d34e032a993d20f06
alpianoor352-ux <alpianoor352@gmail.com>
2026-07-27 11:54:04 +0800

    Update nokos-price.js

--- api/nokos-price.js
+++ api/nokos-price.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     } = req.query;
 
     const response = await fetch(
-      `https://nokos.co.id/api/?action=getPrices&service=${service}&country=${country}&server=${server}`,
+      `https://nokos.co.id/api/?action=getAvailability&service=${service}&country=${country}&server=${server}`,
       {
         headers: {
           "X-API-Key": process.env.NOKOS_API_KEY
@@ -30,4 +30,4 @@ export default async function handler(req, res) {
 
   }
 
-}
+      }
