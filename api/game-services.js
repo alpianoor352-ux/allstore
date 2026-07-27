0ac78452c476b20c183e729b017574ffaaacf3a6
alpianoor352-ux <alpianoor352@gmail.com>
2026-07-28 07:10:51 +0800

    Update game-services.js

--- api/game-services.js
+++ api/game-services.js
@@ -1,39 +1,5 @@
-import crypto from "crypto";
-
 export default async function handler(req, res) {
-  try {
-    const apiId = process.env.SOSMED_API_ID;
-    const apiKey = process.env.SOSMED_API_KEY;
-
-    const sign = crypto
-      .createHash("md5")
-      .update(apiId + apiKey)
-      .digest("hex");
-
-    const body = new URLSearchParams({
-      key: apiKey,
-      sign: sign
-    });
-
-    const response = await fetch(
-      "https://panel.sosmedmedia.com/api/profile",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body
-      }
-    );
-
-    const text = await response.text();
-
-    return res.status(200).send(text);
-
-  } catch (err) {
-    return res.status(500).json({
-      result: false,
-      message: err.message
-    });
-  }
+  return res.json({
+    test: "API jalan"
+  });
 }
