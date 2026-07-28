67265acbd30e43245718afb847c81bf83cb03d5c
alpianoor352-ux <alpianoor352@gmail.com>
2026-07-28 08:35:08 +0800

    Update game-services.js

--- api/game-services.js
+++ api/game-services.js
@@ -1,5 +1,17 @@
-export default async function handler(req, res) {
-  return res.json({
-    test: "API jalan"
-  });
-}
+const response = await fetch(
+  "https://panel.sosmedmedia.com/api/profile",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      key: apiKey,
+      sign: sign
+    })
+  }
+);
+
+const text = await response.text();
+
+return res.status(200).send(text);
